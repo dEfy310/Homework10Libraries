9916de730758b843c7e72959dae298f9f2e67b21
dEfy310 <musinov_2004@mail.ru>
2022-04-01 15:37:35 +0300

    Initial Commit

---
+++ src/main/java/pro/sky/java/course2/homework10libraries/Homework10LibrariesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework10libraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework10LibrariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework10LibrariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/homework10libraries/Homework10LibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework10libraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework10LibrariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
